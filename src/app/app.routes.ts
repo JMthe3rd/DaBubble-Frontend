9782ddd22f608d101848da6b47e62860153a81f8
Selina Karlin <karlinselina@gmail.com>
2024-01-12 16:33:50 +0100

    deleting component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent}
 ];
