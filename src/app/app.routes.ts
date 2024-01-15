c211999193475a3a8e786274f832d029dec0f20b
Selina Karlin <karlinselina@gmail.com>
2024-01-15 13:31:12 +0100

    implementing route for Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SelectAvatarComponent } from './select-avatar/select-avatar.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   // **************************************
   // Hier die SignUpComponent mit der Home oder Login Component tauschen
-  { path: '', component: SignUpComponent },
+  { path: '', component: LoginComponent },
 
   //   **************************************
   { path: 'sign-up', component: SignUpComponent },
