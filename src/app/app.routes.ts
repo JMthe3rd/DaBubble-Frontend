8493992dfe4afe1c14a6b259e4f6fb7284babfda
Selina Karlin <karlinselina@gmail.com>
2024-01-17 14:46:21 +0100

    implementing reset-pw

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { SelectAvatarComponent } from './select-avatar/select-avatar.component';
 import { LoginComponent } from './login/login.component';
 import { SendMailPwComponent } from './send-mail-pw/send-mail-pw.component';
+import { ResetPwComponent } from './reset-pw/reset-pw.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'send-mail-pw', component: SendMailPwComponent },
+  { path: 'reset-pw', component: ResetPwComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'select-avatar', component: SelectAvatarComponent },
 ];
