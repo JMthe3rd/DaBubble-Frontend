e9c945492eddb2520f672d307d203e8075255f34
Selina Karlin <karlinselina@gmail.com>
2024-01-18 13:55:21 +0100

    flags and dummy_data for displaying content

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
-export class WorkspaceComponent {
+export class WorkspaceComponent implements OnInit {
+  showChannels: boolean = false;
+  showDMs: boolean = true;
+  constructor(
+    private router: Router
+  ) {}
 
+
+
+  // ########DUMMY#################DUMMY#################DUMMY#########
+  // ########DUMMY#################DUMMY#################DUMMY#########
+
+  user!: {
+    channels: Array<{ name: string }>,
+    directMessages: Array<any> // Ersetzen Sie 'any' durch einen passenden Typ
+  };
+  channels!: Array<{ name: string }>;
+
+
+
+  ngOnInit(){
+    this.user = {
+      channels: [
+        {
+          name: "Entwicklerteam",
+        },
+        {
+          name: "Marketing",
+        },
+        {
+          name: "Vertrieb",
+        },
+      ],
+      directMessages: [
+        {
+          withUser: {
+            firstName: "Daniele",
+            lastName: "Meyer",
+            imageUrl: "path/to/daniele_meyer.jpg"
+          },
+          messageHistory: [
+            { sender: "Daniele Meyer", message: "Hallo, wie geht's?", receiver: this.user,  timestamp: "2024-01-18T09:00:00" },
+            { sender: this.user, message: "Mir geht's gut, danke!", receiver: "Daniele Meyer", timestamp: "2024-01-18T09:05:00" }
+          ]
+        }
+      ]
+    }
+
+    this.channels = this.user.channels;
+  }
+  
 }
